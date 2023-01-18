5d94f119082fd2286d2c3d7f7a9713ce6306f1e9
elimorw <ivan.protska@yandex.ru>
2023-01-18 20:03:29 +0300

    version-2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,18 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-        HelloWorld bean2 =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean2.getMessage());
 
-        Cat benCat =
-                (Cat) applicationContext.getBean("cat");
-        System.out.println(benCat.getMessage());
-        Cat benCat2 =
-                (Cat) applicationContext.getBean("cat");
-        System.out.println(benCat2.getMessage());
+        Cat benCat = (Cat) applicationContext.getBean("cat");
+        Cat benCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Первая пара - " + (bean1 == bean2));
+        System.out.println("Вторая пара - " + (benCat == benCat2));
     }
 }
